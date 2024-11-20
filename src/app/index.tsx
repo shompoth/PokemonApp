31cdb5986bcdca7bb424a0506de0a4b3fe435774
Shompoth KHAN <dadoulan@gmail.com>
2024-11-20 21:02:02 +0100

    Add screens, librairies, update architecture and config

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from "react-native";
+
+const Index = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View>Hello</View>
+    </SafeAreaView>
+  );
+};
+
+export default Index;
---
+++ src/app/pokemon/[id].tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, Text, View } from "react-native";
+
+const Pokemon = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View>
+        <Text>Detail pokemon</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Pokemon;
