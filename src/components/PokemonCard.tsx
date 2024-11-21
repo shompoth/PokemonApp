27ef689896ea69d47590d7009e98e830f5512a19
Shompoth KHAN <dadoulan@gmail.com>
2024-11-21 20:20:57 +0100

    Update Pokemon card and create functions

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,6 +1,6 @@
 import { Link } from "expo-router";
-import { View, Image, Pressable, Text } from "react-native";
-import { getPokemonIdDisplay } from "../functions/pokemon";
+import { View, Image, Text, TouchableOpacity } from "react-native";
+import { getPokemonArtwork, getPokemonIdDisplay } from "../functions/pokemon";
 
 type Props = {
   name: string;
@@ -18,21 +18,21 @@ export const PokemonCard = ({ name, id }: Props) => {
     >
       {/* Keep flex ? for web ? */}
       {/* <Pressable style={{ flex: 1 / 2 }} className="m-2"> */}
-      <Pressable className="w-[46%] m-2">
+      <TouchableOpacity className="w-[46%] m-2">
         <View className="bg-white p-4 rounded-lg shadow relative aspect-square justify-center">
           <Text className="absolute top-2 right-2 font-bold text-xs text-gray-500">
             {getPokemonIdDisplay(id.toString())}
           </Text>
           <Image
             source={{
-              uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+              uri: getPokemonArtwork(id),
             }}
             className="w-4/5 h-4/5 mx-auto"
             resizeMode="contain"
           />
           <Text className="text-center font-medium mt-2">{name}</Text>
         </View>
-      </Pressable>
+      </TouchableOpacity>
     </Link>
   );
 };
--- src/functions/pokemon.ts
+++ src/functions/pokemon.ts
@@ -5,3 +5,21 @@ export const getPokemonId = (url: string): number => {
 export const getPokemonIdDisplay = (id: string): string => {
   return `#${id.padStart(4, "0")}`;
 };
+
+export const getPokemonArtwork = (id: number | string): string => {
+  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+};
+
+export const formatWeight = (weight?: number): string => {
+  if (!weight) {
+    return "";
+  }
+  return (weight / 10).toString().replace(".", ",") + " kg";
+};
+
+export const formatSize = (size?: number): string => {
+  if (!size) {
+    return "";
+  }
+  return (size / 10).toString().replace(".", ",") + " m";
+};
