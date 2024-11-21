b46854e14096a5af84049f660efe78740dc7200f
Shompoth KHAN <dadoulan@gmail.com>
2024-11-21 12:25:13 +0100

    Add styles for Flatlist and update PokemonCard

--- src/components/pokemon/PokemonCard.tsx
+++ src/components/pokemon/PokemonCard.tsx
@@ -15,7 +15,7 @@ const PokemonCard = ({ name, id }: Props) => {
       }}
       asChild
     >
-      <Pressable className="w-[46%] m-2">
+      <Pressable style={{ flex: 1 / 2 }} className="m-2">
         <View className="bg-white p-4 rounded-lg shadow relative aspect-square justify-center">
           <Text className="absolute top-2 right-2 font-bold text-xs text-gray-500">
             #{String(id).padStart(4, "0")}
@@ -24,7 +24,12 @@ const PokemonCard = ({ name, id }: Props) => {
             source={{
               uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
             }}
-            className="w-24 h-24 mx-auto"
+            style={{
+              width: "80%",
+              height: "80%",
+            }}
+            className="mx-auto"
+            resizeMode="contain"
           />
           <Text className="text-center font-medium mt-2">{name}</Text>
         </View>
