b0984e1057d43470043f841bbd0f04bfd2a59c13
Shompoth KHAN <dadoulan@gmail.com>
2024-11-22 10:47:50 +0100

    HeaderShown false

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,7 +7,7 @@ const queryClient = new QueryClient();
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack />
+      <Stack screenOptions={{ headerShown: false }} />
     </QueryClientProvider>
   );
 }
