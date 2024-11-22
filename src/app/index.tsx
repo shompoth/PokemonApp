15c23267c2c3ca08712212462bef4d50d1538cf7
Shompoth KHAN <dadoulan@gmail.com>
2024-11-22 10:47:28 +0100

    Update header and fix Flatlist issues

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import { View, FlatList, ActivityIndicator } from "react-native";
+import { View, FlatList, ActivityIndicator, Image } from "react-native";
 import { PokemonCard } from "../components/PokemonCard";
 import { SearchBar } from "../components/SearchBar";
 import { useInfiniteFetchQuery } from "../hooks/useFetchQuery";
 import { getPokemonId } from "../functions/pokemon";
 import { RootView } from "../components/RootView";
+import { CustomText } from "../components/CustomText";
 
 export default function Index() {
   const [search, setSearch] = useState("");
-
   const { data, isFetching, fetchNextPage } =
     useInfiniteFetchQuery("/pokemon?limit=20");
 
@@ -29,15 +29,31 @@ export default function Index() {
 
   return (
     <RootView>
-      <View className="p-4">
+      <View className="flex-1 p-4">
+        <View className="flex-row mb-2 px-2 items-center">
+          <Image
+            source={require("../../assets/pokemon-logo.png")}
+            className="h-6 w-6 mr-2"
+          />
+          <CustomText variant="xlarge" className="font-bold">
+            Pokédex
+          </CustomText>
+        </View>
         <SearchBar searchText={search} onChange={setSearch} />
         <FlatList
+          style={{ flex: 1 }}
           data={filteredPokemons}
           numColumns={2}
+          columnWrapperStyle={{ justifyContent: "center" }}
           renderItem={({ item }) => (
             <PokemonCard name={item.name} id={item.id} />
           )}
-          ListFooterComponent={isFetching ? <ActivityIndicator /> : null}
+          ListFooterComponent={
+            <>
+              {isFetching && <ActivityIndicator />}
+              <View className="pb-2" />
+            </>
+          }
           onEndReached={search ? null : () => fetchNextPage()}
           keyExtractor={(item) => item.id.toString()}
         />
